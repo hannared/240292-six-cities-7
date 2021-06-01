9681c6a1078494c693c7678543c42670c410baac
Anna K <hanna.korsak@gmail.com>
2021-06-01 16:00:44 +0200

    add places count

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from '../pages/main/main-page';
 
-function App() {
-  return <MainPage />;
+function App(props) {
+  const { places } = props;
+
+  return <MainPage places={places} />;
 }
 
+App.propTypes = {
+  places: PropTypes.array.isRequired,
+};
 export default App;
