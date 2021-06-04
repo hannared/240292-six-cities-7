a4710e604f8b6ac58d9b248e1bbb380d3afbf2a0
Anna K <hanna.korsak@gmail.com>
2021-06-04 16:50:18 +0200

    add pages

--- project/src/components/place-card/place-card.jsx
+++ project/src/components/place-card/place-card.jsx
@@ -24,13 +24,9 @@ function PlaceCard() {
             className="place-card__bookmark-button place-card__bookmark-button--active button"
             type="button"
           >
-            {/* <svg
-                          className="place-card__bookmark-icon"
-                          width="18"
-                          height="19"
-                        >
-                          <use xlink:href="#icon-bookmark"></use>
-                        </svg> */}
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
             <span className="visually-hidden">In bookmarks</span>
           </button>
         </div>
