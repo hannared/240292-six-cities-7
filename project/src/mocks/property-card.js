9e2812f7aa5437a08dbcc5510eebe29d33d76a5f
Anna K <hanna.korsak@gmail.com>
2021-06-08 23:51:16 +0200

    add mocks

---
+++ project/src/mocks/property-card.js
@@ -0,0 +1,47 @@
+const propertyCard = [
+  {
+    id: '1',
+    photo: 'room.jpg',
+    isPremium: true,
+    price: 60,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    isFavorite: true,
+    rating: '5',
+  },
+
+  {
+    id: '2',
+    photo: 'room.jpg',
+    isPremium: false,
+    price: 100,
+    title: 'New room in the city center',
+    type: 'room',
+    isFavorite: true,
+    rating: '4',
+  },
+
+  {
+    id: '3',
+    photo: 'room.jpg',
+    isPremium: true,
+    price: 150,
+    title: 'Top rated hotel in the heart of the city',
+    type: 'hotel',
+    isFavorite: false,
+    rating: '3',
+  },
+
+  {
+    id: '4',
+    photo: 'room.jpg',
+    isPremium: true,
+    price: 300,
+    title: 'Home sweet Home',
+    type: 'house',
+    isFavorite: true,
+    rating: '5',
+  },
+];
+
+export default propertyCard;
---
+++ project/src/mocks/reviews.js
@@ -0,0 +1,70 @@
+// Аватар автора.
+// Имя автора.
+// Оценку. Оценка выводится в виде закрашенных звезд. Максимальное количество звёзд — 5.
+// Дата отзыва в формате: Месяц Год. Например: April 2019.
+// Текст отзыва.
+
+import dayjs from 'dayjs';
+import dayjsRandom from 'dayjs-random';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+dayjs.extend(dayjsRandom);
+
+const AVATAR_URL = 'https://i.pravatar.cc/54';
+
+const reviews = [
+  {
+    propertyId: '1',
+    id: '1',
+    comment:
+      'Pretium viverra suspendisse potenti nullam ac tortor. Tortor posuere ac ut consequat. Placerat orci nulla pellentesque dignissim enim. Pretium nibh ipsum consequat nisl vel pretium lectus. Suspendisse ultrices gravida dictum fusce.',
+    date: dayjs.between('2018-06-10', '2021-07-06').format('MMMM D'),
+    user: {
+      id: 1,
+      avatar: `${AVATAR_URL}?rnd=${Math.random()}`,
+      name: 'John Doe',
+      rating: '4',
+    },
+  },
+
+  {
+    propertyId: '1',
+    id: '2',
+    comment:
+      'Pretium viverra suspendisse potenti nullam ac tortor. Tortor posuere ac ut consequat.',
+    date: dayjs.between('2018-06-10', '2021-07-06').format('MMMM D'),
+    user: {
+      avatar: `${AVATAR_URL}?rnd=${Math.random()}`,
+      name: 'Alex Clark',
+      rating: '5',
+    },
+  },
+
+  {
+    propertyId: '1',
+    id: '3',
+    text: 'Pretium nibh ipsum consequat nisl vel pretium lectus. Suspendisse ultrices gravida dictum fusce.',
+    date: dayjs.between('2018-06-10', '2021-07-06').format('MMMM D'),
+    user: {
+      avatar: `${AVATAR_URL}?rnd=${Math.random()}`,
+      name: 'Andy Moore',
+      rating: '3',
+    },
+  },
+
+  {
+    propertyId: '1',
+    id: '4',
+    comment:
+      'Amet cursus sit amet dictum. Gravida in fermentum et sollicitudin ac orci phasellus egestas tellus. Scelerisque felis imperdiet proin fermentum leo vel orci porta non. ',
+    date: dayjs.between('2018-06-10', '2021-07-06').format('MMMM D'),
+    user: {
+      avatar: `${AVATAR_URL}?rnd=${Math.random()}`,
+      name: 'Davide Bloom',
+      rating: '5',
+    },
+  },
+];
+
+export default reviews;
