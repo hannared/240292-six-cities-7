2b7f759d1872956c8ca0082cf62db1657fe38da0
Anna K <hanna.korsak@gmail.com>
2021-06-11 22:13:50 +0200

    fix link to favorite

--- project/src/components/pages/property/property-page.jsx
+++ project/src/components/pages/property/property-page.jsx
@@ -5,6 +5,7 @@ import { OfferType, ReviewType } from '../../../types';
 import Logo from '../../logo';
 import PropertyCard from '../../property-card/property-card';
 import ReviewsSection from './reviews-section';
+import Navigation from '../../header/navigation';
 
 function PropertyPage(props) {
   // const { id } = props.match.params;
@@ -27,26 +28,7 @@ function PropertyPage(props) {
         <div className="container">
           <div className="header__wrapper">
             <Logo />
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="/#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <Navigation />
           </div>
         </div>
       </header>
