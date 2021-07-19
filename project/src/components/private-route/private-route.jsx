37648a64d3a698a47220d9ad35d5fbab43a6a346
Anna K <hanna.korsak@gmail.com>
2021-07-19 14:00:47 +0300

    add browser-history component

--- project/src/components/private-route/private-route.jsx
+++ project/src/components/private-route/private-route.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 function PrivateRoute({ render, path, exact, authorizationStatus }) {
   return (
